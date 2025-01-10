2e99b5c82e4d6a79bfcb8246826e957f815d0a5c
reenanrs <115711443+reenanrs1@users.noreply.github.com>
2025-01-10 11:16:07 -0300

    correção

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+    import react from '@vitejs/plugin-react';
 
-// Replace 'your-repo-name' with your actual repository name
-const repoName = 'your-repo-name';
+    // Replace 'your-repo-name' with your actual repository name
+    const repoName = 'todolist-spartacus';
 
-export default defineConfig({
-  plugins: [react()],
-  base: `/${repoName}/`,
-});
+    export default defineConfig({
+      plugins: [react()],
+      base: `/${repoName}/`,
+    });
