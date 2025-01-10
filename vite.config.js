3abb76510e5561ce47dbc38529d7479fb3f0e23b
reenanrs <115711443+reenanrs1@users.noreply.github.com>
2025-01-10 11:22:48 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
     import react from '@vitejs/plugin-react';
 
-    // Replace 'your-repo-name' with your actual repository name
-    const repoName = 'todolist-spartacus';
-
     export default defineConfig({
       plugins: [react()],
-      base: `/${repoName}/`,
+      base: './',
     });
